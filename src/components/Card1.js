3d90606376bff0f1bcc4b82ea87a3f76680269ad
Ekadarshini Routray <100223975+Ekadarshini7272@users.noreply.github.com>
2024-03-08 14:25:14 +0530

    UI Modification

--- src/components/Card1.js
+++ src/components/Card1.js
@@ -15,7 +15,7 @@ const Card1 = () => {
     <Card className='content-box1'>
       <Card.Body className="text-center">
         <img src={myImage} height={'100'} width={'100'}/>
-        <h1>Card1</h1>
+        <h3>CardCoponent1</h3>
         <Card.Text>
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
                </Card.Text>
